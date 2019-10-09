e474143af356f7379a5e38a70b8473d8f563a1c8
Aleksei Kunts <instant.sc@gmail.com>
2019-10-10 02:38:13 +0700

    add enumeration request prototype

--- src/GitLabApiClient/Internal/Http/GitLabApiPagedRequestor.cs
+++ src/GitLabApiClient/Internal/Http/GitLabApiPagedRequestor.cs
@@ -7,6 +7,13 @@ using GitLabApiClient.Internal.Utilities;
 
 namespace GitLabApiClient.Internal.Http
 {
+    internal enum PageFetchStrategy
+    {
+        SinglePage,
+        Total,
+        Next
+    }
+
     internal sealed class GitLabApiPagedRequestor
     {
         private const int MaxItemsPerPage = 100;
@@ -15,6 +22,72 @@ namespace GitLabApiClient.Internal.Http
 
         public GitLabApiPagedRequestor(GitLabApiRequestor requestor) => _requestor = requestor;
 
+        public async IAsyncEnumerable<IList<T>> GetPageEnumeration<T>(string url, int bufferedPages = 3, int firstPage = 1)
+        {
+            if (bufferedPages < 1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(bufferedPages), "buffered page count must be positive");
+            }
+
+            if (firstPage < 1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(firstPage), "first page index must be positive");
+            }
+
+            var (firstResult, headers) = await _requestor.GetWithHeaders<IList<T>>(GetPagedUrl(url, firstPage));
+            var (strategy, totalPages) = GetFetchStrategy(headers);
+            switch (strategy)
+            {
+                //return await GetNextPageList(url, 2, result);
+                case PageFetchStrategy.SinglePage:
+                    yield return firstResult;
+                    yield break;
+                case PageFetchStrategy.Next:
+                {
+                    var request = Task.FromResult((result: firstResult, headers));
+                    while (request != null)
+                    {
+                        var currentResult = await request;
+                        int nextPage = currentResult.headers.GetFirstHeaderValueOrDefault<int>("X-Total-Pages");
+                        if (nextPage > 1)
+                        {
+                            string pagedUrl = GetPagedUrl(url, nextPage);
+
+                            request = _requestor.GetWithHeaders<IList<T>>(pagedUrl);
+                        }
+                        else
+                        {
+                            request = null;
+                        }
+
+                        yield return currentResult.result;
+                    }
+
+                    yield break;
+                }
+                case PageFetchStrategy.Total:
+                {
+                    var requestQueue = new Queue<Task<IList<T>>>();
+                    int nextFetchedPageIndex = 2;
+                    requestQueue.Enqueue(Task.FromResult(firstResult));
+                    while (requestQueue.Count != 0)
+                    {
+                        var currentResult = requestQueue.Dequeue();
+                        while (requestQueue.Count < bufferedPages && nextFetchedPageIndex <= totalPages.Value)
+                        {
+                            string pagedUrl = GetPagedUrl(url, nextFetchedPageIndex);
+                            requestQueue.Enqueue(_requestor.Get<IList<T>>(pagedUrl));
+                            nextFetchedPageIndex++;
+                        }
+
+                        yield return await currentResult;
+                    }
+
+                    yield break;
+                }
+            }
+        }
+
         public async Task<IList<T>> GetPagedList<T>(string url)
         {
             var result = new List<T>();
@@ -22,6 +95,22 @@ namespace GitLabApiClient.Internal.Http
             //make first request and it will get available pages in the headers
             var (results, headers) = await _requestor.GetWithHeaders<IList<T>>(GetPagedUrl(url, 1));
             result.AddRange(results);
+            var (strategy, totalPages) = GetFetchStrategy(headers);
+            switch (strategy)
+            {
+                case PageFetchStrategy.Next:
+                    return await GetNextPageList(url, 2, result);
+                case PageFetchStrategy.SinglePage:
+                    return result;
+                case PageFetchStrategy.Total:
+                    return await GetTotalPagedList(url, totalPages.Value, result);
+                default:
+                    throw new Exception("Unknown fetch strategy");
+            }
+        }
+
+        private (PageFetchStrategy, int? totalPageNumbers) GetFetchStrategy(HttpResponseHeaders headers)
+        {
             int totalPages = headers.GetFirstHeaderValueOrDefault<int>("X-Total-Pages");
             int nextPage = headers.GetFirstHeaderValueOrDefault<int>("X-Next-Page");
 
@@ -29,12 +118,12 @@ namespace GitLabApiClient.Internal.Http
             {
                 // X-Total-Pages is not always present due to performance concern so we have to take the slow path of nextPage
                 case 0 when nextPage > 1:
-                    return await GetNextPageList(url, nextPage, result);
+                    return (PageFetchStrategy.Next, null);
                 case 0:
                 case 1:
-                    return result;
+                    return (PageFetchStrategy.SinglePage, null);
                 default:
-                    return await GetTotalPagedList(url, totalPages, result);
+                    return (PageFetchStrategy.Total, totalPages);
             }
         }
 
